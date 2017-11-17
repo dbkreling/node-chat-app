5f1d3b34dbaf47a62af67d42fb07f0bec23d9bbb
Daniel Kreling <dbkreling@gmail.com>
2017-11-17 18:17:10 -0200

    server: create newMessage on createMessage
    This change is to make .emit be called from io instead of socket. This way
    the message will be broadcasted to all sockets connected to the app, and
    not only one.
    
    Ref.: Lecture 110
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'Barb and Den',
-        text: 'How are you guys doing?'
-    });
 });
 
 socket.on('disconnect', function () {
