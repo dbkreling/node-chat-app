b619f32ee36f21f0ab5c6dd42a4d249ae943876d
Daniel Kreling <dbkreling@gmail.com>
2017-11-28 07:50:38 -0200

    chat-app: add mustache.js for message templates
    Create message templates using mustache.js so that messages are handled in
    a dynamic way and code is improved.
    
    Ref.: Lecture 119
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -10,22 +10,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('Got new message', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    var template = jQuery('#message-template').html(); // select the template from index.html
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    }); // will send it to the browser with a jQuery call
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html); // send it to be rendered by the browser
 });
 
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target=_blank>My current location</a>');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        url: message.url
+    });
 
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
