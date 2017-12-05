c630da185200161b520ac389f10a53efb6476b76
Daniel Kreling <dbkreling@gmail.com>
2017-12-05 07:27:45 -0200

    users: add users' manipulation methods
    This commit introduces the implementation of removeUser() and getUser() methods
    in order to manipulate the users list.
    It also implements tests to make sure they are working as expected.
    
    Ref.: Lecture 125
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- server/utils/users.js
+++ server/utils/users.js
@@ -14,6 +14,25 @@ class Users {
         this.users.push(user);
         return user;
     }
+    removeUser (id) {
+        //return user that was removed
+        var user = this.getUser(id);
+
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id)
+        }
+
+        return user;
+    }
+    getUser (id) {
+        return this.users.filter((user) => user.id === id)[0]; // return the id
+    }
+    getUserList (room) {
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+
+        return namesArray;
+    }
 }
 
 module.exports = {Users};
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,6 +2,25 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+    var users;
+
+    beforeEach(() => {
+        users = new Users();
+        users.users = [{
+            id: '1',
+            name: 'Necas',
+            room: 'Node Course'
+        }, {
+            id: '2',
+            name: 'Klaus',
+            room: 'React Course'
+        }, {
+            id: '3',
+            name: 'Daniel',
+            room: 'Node Course'
+        }];
+    });
+
     it('should add new user', () => {
         var users = new Users();
         var user = {
@@ -12,5 +31,47 @@ describe('Users', () => {
         users.addUser(user.id, user.name, user.room);
 
         expect(users.users).toEqual([user]);
+    });
+
+    it('should remove a user', () => {
+        var userId = '3';
+        var user = users.removeUser(userId);
+
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    });
+
+    it('should not remove a user', () => {
+        var userId = '44';
+        var user = users.removeUser(userId);
+
+        expect(user).toNotExist();
+        expect(users.users.length).toBe(3);
+    });
+
+    it('should find user', () => {
+        var userId = '3';
+        var user = users.getUser(userId);
+
+        expect(user.id).toBe(userId);
+    });
+
+    it('should not find a user', () => {
+        var userId = '44';
+        var user = users.getUser(userId);
+
+        expect(user).toNotExist();
+    });
+
+    it('should return names for Node Course', () => {
+        var userList = users.getUserList('Node Course');
+
+        expect(userList).toEqual(['Necas', 'Daniel']);
+    });
+
+    it('should return names for React Course', () => {
+        var userList = users.getUserList('React Course');
+
+        expect(userList).toEqual(['Klaus']);
     });
 })
